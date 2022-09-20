0de08720af0f4f131a51ce08d39136b9be0baa26
Justin Lam <justin.kh.lam@gmail.com>
2022-09-20 00:39:29 -0400

    add in user model. add in bcrypt. add salt/hash

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-  // console.log(req.body);
   try {
-    const newUser = new User(req.body);
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    // spread to take everything in body
+    const newUser = new User({ ...req.body, password: hash });
   } catch (error) {}
 };
