7eddfbb6c32aa8c6197edf6616f130e78b06ad16
Justin Lam <justin.kh.lam@gmail.com>
2022-09-19 22:55:45 -0400

    install dotenv. set up dotenv/port and app.listen

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+app.listen(8080, () => {
+  console.log(`Server connected at ${port}! 🐅`);
+});
