9b9e604fb17f4342d51c96c43113f4d14733a514
Justin Lam <justin.kh.lam@gmail.com>
2022-09-19 23:31:36 -0400

    add in connection for Mongoose

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
 
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGODB)
+    .then(() => {
+      console.log(`MongoDB connected! 🐯`);
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
 const port = process.env.PORT || 8000;
 
 app.listen(8080, () => {
+  connect();
   console.log(`Server connected at ${port}! 🐅`);
 });
