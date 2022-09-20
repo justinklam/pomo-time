19dde15134550f599ae4527b2ccb506a9b988f6e
Justin Lam <justin.kh.lam@gmail.com>
2022-09-20 00:40:36 -0400

    add in user save and response message and status

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,5 +8,8 @@ export const signup = async (req, res) => {
     const hash = bcrypt.hashSync(req.body.password, salt);
     // spread to take everything in body
     const newUser = new User({ ...req.body, password: hash });
+
+    await newUser.save();
+    res.status(200).send("User created!");
   } catch (error) {}
 };
