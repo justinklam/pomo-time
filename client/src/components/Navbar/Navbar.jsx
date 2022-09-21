917d9ab186bf3de9f7ceef919ae11351902d8959
Justin Lam <justin.kh.lam@gmail.com>
2022-09-21 16:13:36 -0400

    add logo and links. add in base css

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,15 +2,29 @@ import React from "react";
 import "./navbar.css";
 
 // Image Assets
-import Logo from "../../img/logo.png";
+// import Logo from "../../img/logo.png";
 
 const Navbar = () => {
   return (
-    <>
+    <nav>
       <div className="logo">
-        <img src={Logo} alt="Logo" />
+        <a href="/">
+          <i class="fa-solid fa-clock"></i>
+          <h4>Pomo-Time</h4>
+        </a>
       </div>
-    </>
+      <ul className="navlinks">
+        <li className="link">
+          <a href="/">Home</a>
+        </li>
+        <li className="link">
+          <a href="/">Timer</a>
+        </li>
+        <li className="link">
+          <a href="/">Register</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
