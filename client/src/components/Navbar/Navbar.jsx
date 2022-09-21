dee0d60b258d581d23c7be33d2c9bf35a899fa7e
Justin Lam <justin.kh.lam@gmail.com>
2022-09-21 14:15:26 -0400

    set up navbar component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className>Navbar</div>;
+    </>
+  );
 };
 
 export default Navbar;
