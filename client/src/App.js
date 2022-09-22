2eacaa38b0a4a723850071df97f9b2bba967efbe
Justin Lam <justin.kh.lam@gmail.com>
2022-09-22 19:00:06 -0400

    import routes/route from react-router-dom. set up routes for all main nav components. import pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
 import "./app.css";
 // React Router Dom
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Components
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import Timer from "./pages/Timer";
+import Contact from "./pages/Contact";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/timer">
+          <Route index element={<Timer />} />
+        </Route>
+        <Route path="/contact">
+          <Route index element={<Contact />} />
+        </Route>
+        <Route path="/sign-up">
+          <Route index element={<SignUp />} />
+        </Route>
+      </Routes>
       <div className="main">Main Area</div>
     </BrowserRouter>
   );
