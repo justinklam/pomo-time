ffeceb5d9d228b908f66c1aee438c621912be4be
Justin Lam <justin.kh.lam@gmail.com>
2022-09-22 11:36:33 -0400

    wrap app in browserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import "./app.css";
-
+// React Router Dom
+import { BrowserRouter } from "react-router-dom";
 // Components
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <div className="main">Main Area</div>
-    </>
+    </BrowserRouter>
   );
 }
 
