bf8b6025bf6dd1f0fb788f542dd0ba2d68497a18
Justin Lam <justin.kh.lam@gmail.com>
2022-09-23 00:06:24 -0400

    update file structure

---
+++ client/src/components/pages/SignUp/SignUp.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./signup.css";
+
+const SignUp = () => {
+  return <div>SignUp</div>;
+};
+
+export default SignUp;
---
+++ client/src/components/pages/Timer/Timer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./timer.css";
+
+const Timer = () => {
+  return <div>Timer</div>;
+};
+
+export default Timer;
