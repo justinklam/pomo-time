65c1890d011fdd160fe6a4d31aacd826b6610198
Justin Lam <justin.kh.lam@gmail.com>
2022-09-23 20:04:15 -0400

    import in circular progress bar and baase colours

--- client/src/components/pages/Timer/Timer.jsx
+++ client/src/components/pages/Timer/Timer.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./timer.css";
 
+// React Circular Progress bar
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+// Components
+import PlayButton from "../../PlayButton/PlayButton";
+
 const Timer = () => {
-  return <div>Timer</div>;
+  const percentage = 60;
+  const red = "#f54e4e";
+  const green = "#4aec8c";
+
+  return (
+    <div>
+      <CircularProgressbar
+        value={percentage}
+        styles={buildStyles({
+          textColor: "#fff",
+          pathColor: red,
+          tailColor: "rgba(255, 255, 255, .2) ",
+        })}
+        text={`${percentage}%`}
+      />
+      <PlayButton />
+    </div>
+  );
 };
 
 export default Timer;
