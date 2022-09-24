282bcc5b11efdcd5cdba749608b764c20c6ee84c
Justin Lam <justin.kh.lam@gmail.com>
2022-09-24 13:31:17 -0400

    add in break slider. modify values

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -5,16 +5,25 @@ import ReactSlider from "react-slider";
 const Settings = () => {
   return (
     <div className="settings-container">
-      <label className="pomodoro-label">Work Time:</label>
+      <label className="pomodoro-label">Work Duration - Minutes:</label>
       <ReactSlider
         className="slider"
         thumbClassName="slider-thumb"
         trackClassName="slider-track"
-        value={45}
+        marksClassName="marks"
+        value={5}
         min={1}
-        max={120}
+        max={60}
       ></ReactSlider>
-      <label className="break-label">Break Time:</label>
+      <label className="pomodoro-label">Break Duration - Minutes:</label>
+      <ReactSlider
+        className="slider-break"
+        thumbClassName="slider-thumb"
+        trackClassName="slider-track"
+        value={5}
+        min={1}
+        max={60}
+      ></ReactSlider>
     </div>
   );
 };
