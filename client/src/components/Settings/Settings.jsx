b5bf3a2556252d92131a67b167af960667340212
Justin Lam <justin.kh.lam@gmail.com>
2022-09-24 13:14:34 -0400

    add in ReactSlider. add in base styling for label and slider

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./settings.css";
+import ReactSlider from "react-slider";
 
 const Settings = () => {
   return (
     <div className="settings-container">
       <label className="pomodoro-label">Work Time:</label>
+      <ReactSlider
+        className="slider"
+        thumbClassName="slider-thumb"
+        trackClassName="slider-track"
+        value={45}
+        min={1}
+        max={120}
+      ></ReactSlider>
       <label className="break-label">Break Time:</label>
     </div>
   );
