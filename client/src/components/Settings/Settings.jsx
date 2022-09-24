a255b5a68272dc99b2deaaeb331825b95b5fabfd
Justin Lam <justin.kh.lam@gmail.com>
2022-09-24 13:02:57 -0400

    add initial elements for Settings

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./settings.css";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className="settings-container">
+      <label className="pomodoro-label">Work Time:</label>
+      <label className="break-label">Break Time:</label>
+    </div>
+  );
 };
 
 export default Settings;
