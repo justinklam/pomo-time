38dd7be45d919a45e183532487e02fa7d8d32025
Justin Lam <justin.kh.lam@gmail.com>
2022-09-26 23:49:09 -0400

    add in title, form, input fields for login elements

--- client/src/components/pages/Login/Login.jsx
+++ client/src/components/pages/Login/Login.jsx
@@ -2,7 +2,34 @@ import React from "react";
 import "./login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login-container">
+      <h1 className="login-title">Login</h1>
+      <h2 className="login-subtitle">to track your stats!</h2>
+      <form className="login-form">
+        <input placeholder="Username" className="login-input"></input>
+        <input
+          type="password"
+          placeholder="Password"
+          className="login-input"
+        ></input>
+        <button className="login-button">Login</button>
+      </form>
+
+      <h2 className="login-subtitle">or</h2>
+      <h1 className="login-title">Sign Up</h1>
+      <form className="login-form">
+        <input placeholder="Username" className="login-input"></input>
+        <input type="email" placeholder="Email" className="login-input"></input>
+        <input
+          type="password"
+          placeholder="Password"
+          className="login-input"
+        ></input>
+        <button className="login-button">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
