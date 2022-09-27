9e33cc2ea964b379c67e7ded6a97b0149f3181cc
Justin Lam <justin.kh.lam@gmail.com>
2022-09-27 15:55:34 -0400

    add in useState for name/email/password. add in onClick handlers for buttons and input fields

--- client/src/components/pages/Login/Login.jsx
+++ client/src/components/pages/Login/Login.jsx
@@ -1,32 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
 const Login = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+  };
+
   return (
     <div className="login-container">
       <h1 className="login-title">Login</h1>
       <h2 className="login-subtitle">to track your stats!</h2>
       <form className="login-form">
-        <input placeholder="Username" className="login-input"></input>
+        <input
+          placeholder="Username"
+          className="login-input"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
         <input
           type="password"
           placeholder="Password"
           className="login-input"
+          onChange={(e) => setPassword(e.target.value)}
         ></input>
-        <button className="login-button">Login</button>
+        <button className="login-button" onClick={handleLogin}>
+          Login
+        </button>
       </form>
 
       <h2 className="login-subtitle">or</h2>
       <h1 className="login-title">Sign Up</h1>
       <form className="login-form">
-        <input placeholder="Username" className="login-input"></input>
-        <input type="email" placeholder="Email" className="login-input"></input>
+        <input
+          placeholder="Username"
+          className="login-input"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <input
+          type="email"
+          placeholder="Email"
+          className="login-input"
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
         <input
           type="password"
           placeholder="Password"
           className="login-input"
+          onChange={(e) => setPassword(e.target.value)}
         ></input>
-        <button className="login-button">Login</button>
+        <button className="login-button" onClick={handleRegister}>
+          Sign Up
+        </button>
       </form>
     </div>
   );
