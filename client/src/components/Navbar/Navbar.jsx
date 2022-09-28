9ca5d918d01c19bf9769e9a4a3de56d73acf42ff
Justin Lam <justin.kh.lam@gmail.com>
2022-09-27 22:14:27 -0400

    add in userContext into Navbar component. add conditional rendering for login/logout button. add logout onClick handlers

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./navbar.css";
 // React Router Dom
 import { Link } from "react-router-dom";
 
+// Context
+import UserContext from "../../utils/UserContext";
+
 const Navbar = () => {
   const [click, setClick] = useState(false);
 
+  const { user, setUser } = useContext(UserContext);
+  // console.log("nav-user", user);
+
   // sets opposite of current click status
   const handleClick = () => setClick(!click);
-
   const closeMobileMenu = () => setClick(false);
 
   return (
@@ -17,7 +22,6 @@ const Navbar = () => {
         Pomo-Time
       </Link>
       <div className="menu-icon" onClick={handleClick}>
-        {/* if click is true, show fas fa-times else hamburger */}
         <i className={click ? "fas fa-times" : "fas fa-bars"} />
       </div>
       <ul className={click ? "nav-menu active" : "nav-menu"}>
@@ -31,29 +35,50 @@ const Navbar = () => {
             Timer <i className="fa-solid fa-clock"></i>
           </Link>
         </li>
-        <li className="nav-item">
-          {/* <Link
-            to="/sign-up"
-            className="nav-links-mobile"
-            onClick={closeMobileMenu}
-          >
-            Sign Up
-          </Link> */}
-          <Link
-            to="/login"
-            className="nav-links-mobile"
-            onClick={closeMobileMenu}
-          >
-            Login
-          </Link>
-        </li>
+
+        {/* Mobile Nav Links */}
+        {!user ? (
+          <li className="nav-item">
+            <Link
+              to="/login"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
+              Login
+            </Link>
+          </li>
+        ) : (
+          <li className="nav-item">
+            <Link
+              to="/timer"
+              className="nav-links-mobile"
+              onClick={() => {
+                closeMobileMenu();
+                setUser(null);
+              }}
+            >
+              Logout
+            </Link>
+          </li>
+        )}
       </ul>
-      {/* <Link to="/sign-up">
-        <button className="sign-up-btn">Sign Up</button>
-      </Link> */}
-      <Link to="/login">
-        <button className="login-btn">Login</button>
-      </Link>
+
+      {!user ? (
+        <Link to="/login">
+          <button className="login-btn">Login</button>
+        </Link>
+      ) : (
+        <Link to="/timer">
+          <button
+            className="login-btn"
+            onClick={() => {
+              setUser(null);
+            }}
+          >
+            Logout
+          </button>
+        </Link>
+      )}
     </nav>
   );
 };
