1e01ffc6d994a79692956f16ca2f81059a43f07e
Justin Lam <justin.kh.lam@gmail.com>
2022-09-28 13:43:49 -0400

    add placeholder for Statistics page

--- client/src/components/pages/Statistics/Statistics.jsx
+++ client/src/components/pages/Statistics/Statistics.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./statistics.css";
 
 const Statistics = () => {
-  return <div>Statistics</div>;
+  return (
+    <div className="statistics-container">
+      <h1 className="statistics-title">Statistics - Placeholder</h1>
+    </div>
+  );
 };
 
 export default Statistics;
