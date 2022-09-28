8551ccdf7881ec49824c50efe139f5ba978cceb7
Justin Lam <justin.kh.lam@gmail.com>
2022-09-27 20:50:00 -0400

    import redux into index.js. initialize store and add in configuration

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// React Redux
+import { Provider } from "react-redux";
+import { persistor, store } from "./components/redux/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
