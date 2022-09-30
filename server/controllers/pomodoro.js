9dec9e13e45de1fb10e16a8de7d5960964eead68
Justin Lam <justin.kh.lam@gmail.com>
2022-09-30 15:09:52 -0400

    add in new mongoose query to create pomodoro

--- server/controllers/pomodoro.js
+++ server/controllers/pomodoro.js
@@ -6,4 +6,7 @@ import Pomodoro from "../models/Pomodoro";
 // Helper Functions
 import { createError } from "../helpers/error";
 
-export const addPomodoro = async (req, res, next) => {};
+export const addPomodoro = async (req, res, next) => {
+  // create a new Pomodoro entry with the userId
+  const newPomodoro = new Pomodoro({ userId: req.user.id });
+};
