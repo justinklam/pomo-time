18f5321b7d13c1828df741880b30795bcdf9b591
Justin Lam <justin.kh.lam@gmail.com>
2022-10-01 11:44:31 -0400

    add in try/catch to save new pomodoro entry

--- server/controllers/pomodoro.js
+++ server/controllers/pomodoro.js
@@ -9,4 +9,8 @@ import { createError } from "../helpers/error";
 export const addPomodoro = async (req, res, next) => {
   // create a new Pomodoro entry with the userId
   const newPomodoro = new Pomodoro({ userId: req.user.id });
+  try {
+    const savedPomodoro = await newPomodoro.save();
+    res.status(200).json(savedPomodoro);
+  } catch (error) {}
 };
